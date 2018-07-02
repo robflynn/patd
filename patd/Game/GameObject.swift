56d24edd72d61b0db4482a268eff1c703a1e1a8e
Rob Flynn <rob@thingerly.com>
2018-07-02 02:47:55 -0400

    Add instance timers

--- patd/Game/GameObject.swift
+++ patd/Game/GameObject.swift
@@ -13,6 +13,11 @@ typealias ObjectID = UUID
 class GameObject: Equatable {
     var Id: ObjectID = ObjectID()
 
+    let instantiatedAt: Date = Date()
+    var instanceAge: TimeInterval {
+        return instantiatedAt.timeIntervalSinceNow
+    }
+
     static func == (lhs: GameObject, rhs: GameObject) -> Bool {
         return lhs.Id.uuidString == rhs.Id.uuidString
     }
